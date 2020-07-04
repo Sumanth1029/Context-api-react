77bfe00f5078866d8e8c663a57dea1f3a40525a9
sumanth <sumanthhegde1029@gmail.com>
2020-07-04 17:37:22 +0530

    3rd commit

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,25 +1,25 @@
-import React,{Component} from 'react';
-import { Themecontext } from '../contexts/Theme';
+import React, { Component, useContext } from "react";
+import { Themecontext } from "../contexts/Theme";
 
-class Booklist extends Component {
-    
-    static contextType=Themecontext;
 
-    render() { 
-        const {islight,light,dark}=this.context
 
-        const theme=islight?light:dark
+const Booklist = () => {
+    const {islight,light,dark}=useContext(Themecontext)
+    const theme = islight ? light : dark;
 
-        return (
-        <div className="book-list" style={{background:theme.bg,color:theme.txtcolor}}> 
-            <ul>
-                <li style={{background:theme.ui}}>book1</li>
-                <li style={{background:theme.ui}}>book2</li>
-                <li style={{background:theme.ui}}>book3</li>
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.txtcolor }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>book1</li>
+        <li style={{ background: theme.ui }}>book2</li>
+        <li style={{ background: theme.ui }}>book3</li>
+      </ul>
+    </div>
+  );
+};
 
-            </ul>
-        </div>  );
-    }
-}
- 
+
 export default Booklist;
