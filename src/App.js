57d1f080f816b371ba03ef44cccaa62c5b91ec62
sumanth <sumanthhegde1029@gmail.com>
2020-07-04 12:45:43 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Booklist from './components/Booklist';
-import Themeprovider from './contexts/Theme';
-import Toggle from './components/Toggle';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Booklist from "./components/Booklist";
+import Themeprovider from "./contexts/Theme";
+import Toggle from "./components/Toggle";
+import AuthProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
-      <Themeprovider>
-     <Navbar/>
-     <Booklist/>
-     <Toggle/>
-     </Themeprovider>
+      <AuthProvider>
+        <Themeprovider>
+          <Navbar />
+          <Booklist />
+          <Toggle />
+        </Themeprovider>
+      </AuthProvider>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import { Themecontext } from '../contexts/Theme';
+import React, { Component } from "react";
+import { Themecontext } from "../contexts/Theme";
+import AuthProvider, { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends Component {
+  render() {
+    return (
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <Themecontext.Consumer>
+            {(context) => {
+             
+             const {isAuth,toggleAuth}=authContext
 
+             const { islight, light, dark } = context;
 
+              const theme = islight ? light : dark;
 
-    render() {
-
-        
-
-
-        return (
-
-
-            <Themecontext.Consumer>{(context) => {
-                const { islight, light, dark } = context
-
-        const theme = islight ? light : dark
-
-                return (
-                    <nav style={{ background: theme.ui, color: theme.txtcolor }}>
-                        <h1>Context</h1>
-                        <ul>
-                            <li>home</li>
-                            <li>home2</li>
-                            <li>home3</li>
-
-
-                        </ul>
-
-                    </nav>)
+              return (
+                <nav style={{ background: theme.ui, color: theme.txtcolor }}>
+                  <h1>Context</h1>
+                  <div onClick={toggleAuth}>
+                      {isAuth?"loggged in":"logged out"}
+                  </div>
+                  <ul>
+                    <li>home</li>
+                    <li>home2</li>
+                    <li>home3</li>
+                  </ul>
+                </nav>
+              );
             }}
-            </Themecontext.Consumer>
-            
-            )
-    }
+          </Themecontext.Consumer>
+        )}
+      </AuthContext.Consumer>
+    );
+  }
 }
 export default Navbar;
